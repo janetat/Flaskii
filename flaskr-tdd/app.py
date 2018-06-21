7ee8937855cc35e5e2e5a323f6a3e74ec5c5c31d
allin <aa704538319@163.com>
2018-06-21 15:13:21 +0800

    flaskr-tdd: pass test3 database setup

--- flaskr-tdd/app.py
+++ flaskr-tdd/app.py
@@ -13,5 +13,37 @@ PASSWORD = 'admin'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+# connect to database
+def connect_db():
+    """ Connects to the database. """
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+# get database connection
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+# init the database
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+# close databse connection
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
 if __name__ == '__main__':
+    init_db()
     app.run()
