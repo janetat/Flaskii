faa9d9b2ab32601e8836d81fd7170932a5f69797
allin <aa704538319@163.com>
2018-06-25 11:21:21 +0800

    add SQLAlchemy

--- flaskr-tdd/app.py
+++ flaskr-tdd/app.py
@@ -1,63 +1,58 @@
 # imports
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
+import os
+
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+# get the folder where this file runs
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # configuration
 DATABASE = 'flaskr.db'
 DEBUG = True
-SECRET_KEY = 'Allen Secret Key'
+SECRET_KEY = 'my_precious'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 
-# create and initialize app
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# database config
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# create app
 app = Flask(__name__)
 app.config.from_object(__name__)
+db = SQLAlchemy(app)
+
+import models
 
 
-# connect to database
-def connect_db():
-    """ Connects to the database. """
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-# get database connection
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-# init the database
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-# close databse connection
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-
-# Routes and view functions
 @app.route('/')
 def index():
     """Searches the database for entries, then displays them."""
-    db = get_db()
-    cur = db.execute('SELECT * FROM entries ORDER BY id DESC')
-    entries = cur.fetchall()
+    entries = db.session.query(models.Flaskr)
     return render_template('index.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Adds new post to the database."""
+    if not session.get('logged_in'):
+        abort(401)
+    new_entry = models.Flaskr(request.form['title'], request.form['text'])
+    db.session.add(new_entry)
+    db.session.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    """User login/authentication/session management"""
+    """User login/authentication/session management."""
     error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
@@ -79,34 +74,20 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    """Add new post to database."""
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute(
-        'INSERT INTO entries (title, text) VALUES (?, ?)',
-        [request.form['title'], request.form['text']]
-    )
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('index'))
-
-@app.route('/delete/<post_id>', methods=['GET'])
+@app.route('/delete/<int:post_id>', methods=['GET'])
 def delete_entry(post_id):
-    """Delete post from database"""
+    """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db = get_db()
-        db.execute('DELETE FROM entries WHERE id=' + post_id)
-        db.commit()
-        result = {'status': 1, 'message': 'Post Deleted'}
+        new_id = post_id
+        db.session.query(models.Flaskr).filter_by(post_id=new_id).delete()
+        db.session.commit()
+        result = {'status': 1, 'message': "Post Deleted"}
+        flash('The entry was deleted.')
     except Exception as e:
         result = {'status': 0, 'message': repr(e)}
-    # 以json的形式返回给前端
     return jsonify(result)
 
+
 if __name__ == '__main__':
-    init_db()
     app.run()
--- flaskr-tdd/schema.sql
+++
@@ -1,6 +0,0 @@
-DROP table if EXISTS entries;
-CREATE table entries (
-  id INTEGER PRIMARY KEY autoincrement,
-  title TEXT NOT NULL,
-  text TEXT NOT NULL
-);
